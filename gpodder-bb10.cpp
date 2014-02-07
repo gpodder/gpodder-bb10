0959227fd15d7fa1daccfcb2be455dcde37ac201
Thomas Perl <m@thp.io>
2014-02-07 01:38:39 +0100

    Font fixes

--- gpodder-bb10.cpp
+++ gpodder-bb10.cpp
@@ -1,10 +1,15 @@
 #include <QGuiApplication>
 #include <QQuickView>
 #include <QFileInfo>
+#include <QFont>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    QFont font("Slate Pro");
+    font.setStretch(103);
+    font.setWeight(30);
+    app.setFont(font);
     QQuickView view;
     view.setSource(QUrl::fromLocalFile(QFileInfo("app/native/gpodder-ui-qml/touch/gpodder.qml").absoluteFilePath()));
     view.setResizeMode(QQuickView::SizeRootObjectToView);
