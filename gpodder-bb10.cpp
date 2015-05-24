45f8cffa0216376b89922d864b0f3cb6a1e5b227
Thomas Perl <m@thp.io>
2015-05-24 17:13:57 +0200

    load libpython globally

--- gpodder-bb10.cpp
+++ gpodder-bb10.cpp
@@ -7,6 +7,9 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    // Load libpython globally (so _sqlite3.so can be loaded)
+    dlopen("/usr/lib/libpython3.2m.so.1.0", RTLD_GLOBAL | RTLD_NOW);
+
     QFontDatabase::addApplicationFont("app/native/gpodder-ui-qml/touch/icons/iconic_fill.ttf");
     QFontDatabase::addApplicationFont("app/native/gpodder-ui-qml/touch/fonts/source-sans-pro.extralight.ttf");
 
